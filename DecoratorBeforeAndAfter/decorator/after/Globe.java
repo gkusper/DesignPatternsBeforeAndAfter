35a7601e8be483aacca4919221a4ee6ae938b23a
gkusper <gkusper@aries.ektf.hu>
2025-04-27 21:54:16 +0200

    Update Globe.java

--- DecoratorBeforeAndAfter/decorator/after/Globe.java
+++ DecoratorBeforeAndAfter/decorator/after/Globe.java
@@ -3,7 +3,7 @@ package decorator.after;
 public class Globe extends Decorator {
     public Globe(XmasTree comp) { super(comp); }
     @Override
-    public String decorToString() { return " with a glob"; }
+    public String decorToString() { return " with a globe"; }
     @Override
     public double decorGetPrice() { return 20.0; }
 }
